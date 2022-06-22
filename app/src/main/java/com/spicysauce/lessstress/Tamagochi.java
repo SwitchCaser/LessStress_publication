f4bfdbf8beade901d1ba278835ba86addcc426b7
SaikyoNoSentoki <50771595+SwitchCaser@users.noreply.github.com>
2022-06-22 14:42:58 +0300

    ignor this

--- app/src/main/java/com/spicysauce/lessstress/Tamagochi.java
+++ app/src/main/java/com/spicysauce/lessstress/Tamagochi.java
@@ -56,6 +56,7 @@ public class Tamagochi extends AppCompatActivity {
             }
         });
         */
+        //issues found
         lamp.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
